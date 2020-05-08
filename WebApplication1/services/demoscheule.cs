035001784a67129c6915266276d537bfed003b08
nyc <1139800084@qq.com>
2020-05-08 15:39:48 +0800

    定时任务

---
+++ WebApplication1/services/demoscheule.cs
@@ -0,0 +1,33 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.services
+{
+    public class demoscheule
+    {
+        public static void start()
+        {
+            ISchedulerFactory schedulefactory = new StdSchedulerFactory();//实例化调度器工厂
+            IScheduler scheduler = schedulefactory.GetScheduler();//实例化调度器
+            scheduler.Start();
+            IJobDetail job1 = JobBuilder.Create<quartzdemo>()//创建一个作业
+                .WithIdentity("demojob1", "groupa")
+                .Build();
+
+            ITrigger trigger1 = TriggerBuilder.Create()//创建一个触发器
+                .WithIdentity("demotrigger1", "groupa")
+                .StartNow()
+                .WithSimpleSchedule(b => b.WithIntervalInSeconds(5)//5秒执行一次
+                .RepeatForever())//无限循环执行
+                .Build();
+ 
+            scheduler.ScheduleJob(job1, trigger1);//把作业，触发器加入调度器
+
+
+        }
+    }
+}
---
+++ WebApplication1/services/quartzdemo.cs
@@ -0,0 +1,16 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.services
+{
+    public class quartzdemo:IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("execute job");
+        }
+    }
+}
